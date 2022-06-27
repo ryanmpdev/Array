7ee50487fb3257c7e5e7381a360812861a3c7a93
ryanmpdev <ryanmaciel697@gmail.com>
2022-06-27 11:11:03 -0300

    array

---
+++ script.js
@@ -0,0 +1,52 @@
+//QUestão 1
+
+var animais = ['Cavalo', 'Cachorro', 'Gato', 'Boi', 'Papagaio']
+
+console.log(animais)
+
+
+//Questão 2
+
+let frutas = ['manga', 'maçã', 'banana']
+
+  frutas.unshift("laranja")
+
+   console.log(frutas)
+
+
+   //questão 3
+
+   let esportes = ['futebol', 'tênis', 'basquete', ' vôlei']
+
+   esportes.pop()
+
+   console.log(esportes)
+
+
+   //questão 4
+
+   let familia = ['mãe','pai','irmãos','avós',]
+
+   familia.push("tios", "primos")
+
+   console.log(familia)
+
+   //questão 5
+
+   let pais = ['argentina', 'brasil','peru', 'méxico', 'frança']
+   
+   pais.shift()
+
+   console.log(pais)
+
+
+   //questão 6
+
+    let numbers = [7,5,6,3,8,9,2,1,4];
+    console.log(numbers);
+
+     numbers.sort(function(a, b){
+     return a - b;
+ });
+ console.log(numbers);
+
